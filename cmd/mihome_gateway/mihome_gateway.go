2a960a59e466f27875179b127486573d9938abea
David Thorpe <djt@mutablelogic.com>
2018-04-07 11:00:17 +0100

    Updated client code

--- cmd/mihome_gateway/mihome_gateway.go
+++ cmd/mihome_gateway/mihome_gateway.go
@@ -10,11 +10,7 @@
 package main
 
 import (
-	"context"
 	"os"
-	"sync"
-
-	"github.com/djthorpe/sensors"
 
 	// Frameworks
 	"github.com/djthorpe/gopi"
@@ -26,103 +22,6 @@ import (
 	// RPC Services
 	_ "github.com/djthorpe/sensors/cmd/mihome_gateway/service"
 )
-
-////////////////////////////////////////////////////////////////////////////////
-
-type Command uint
-
-const (
-	COMMAND_RESET Command = iota
-)
-
-////////////////////////////////////////////////////////////////////////////////
-
-var (
-	cancel   context.CancelFunc
-	lock     sync.Mutex
-	commands chan Command
-)
-
-////////////////////////////////////////////////////////////////////////////////
-
-func GetContext() context.Context {
-	lock.Lock()
-	defer lock.Unlock()
-
-	// Cancel previously running receive
-	if cancel != nil {
-		cancel()
-	}
-
-	// Create a new context
-	var ctx context.Context
-	ctx, cancel = context.WithCancel(context.Background())
-
-	// Return context
-	return ctx
-}
-
-func Cancel() {
-	lock.Lock()
-	defer lock.Unlock()
-	if cancel != nil {
-		cancel()
-		cancel = nil
-	}
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-func ReceiveLoop(app *gopi.AppInstance, done <-chan struct{}) error {
-	mihome := app.ModuleInstance("sensors/mihome").(sensors.MiHome)
-	events := mihome.Subscribe()
-FOR_LOOP:
-	for {
-		select {
-		case <-done:
-			break FOR_LOOP
-		case evt := <-events:
-			if ot_evt, ok := evt.(sensors.OTEvent); ot_evt != nil && ok {
-				app.Logger.Info("Received event: %v", evt)
-			}
-		}
-	}
-
-	// Stop listening for messages
-	mihome.Unsubscribe(events)
-
-	// Finished
-	return nil
-}
-
-func CommandLoop(app *gopi.AppInstance, done <-chan struct{}) error {
-	mihome := app.ModuleInstance("sensors/mihome").(sensors.MiHome)
-FOR_LOOP:
-	for {
-		select {
-		case <-done:
-			break FOR_LOOP
-		default:
-			app.Logger.Info("In Receive mode")
-			if err := mihome.Receive(GetContext(), sensors.MIHOME_MODE_MONITOR); err != nil {
-				app.Logger.Error("CommandLoop: %v", err)
-			}
-			app.Logger.Info("End of receive mode")
-		}
-	}
-
-	// Finished
-	return nil
-}
-
-func CommandCancel(app *gopi.AppInstance, done <-chan struct{}) error {
-	// Wait for done and then cancel
-	app.Logger.Debug("CommandCancel waiting for done")
-	<-done
-	app.Logger.Debug("CommandCancel cancelling")
-	Cancel()
-	return nil
-}
 
 ////////////////////////////////////////////////////////////////////////////////
 
