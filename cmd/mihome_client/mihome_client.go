8380e2d1ab090ce0fe06d7e99785a6c8514201c0
David Thorpe <djt@mutablelogic.com>
2018-04-07 15:08:52 +0100

    Updates to the client

--- cmd/mihome_client/mihome_client.go
+++ cmd/mihome_client/mihome_client.go
@@ -15,7 +15,6 @@ import (
 	"io"
 	"os"
 	"reflect"
-	"strings"
 
 	// Frameworks
 	"github.com/djthorpe/gopi"
@@ -29,6 +28,50 @@ import (
 )
 
 ////////////////////////////////////////////////////////////////////////////////
+// GLOBAL VARIABLES
+
+var (
+	start chan pb.MiHomeClient
+)
+
+////////////////////////////////////////////////////////////////////////////////
+// RECEIEVE MESSAGES LOOP
+
+func ReceiveLoop(app *gopi.AppInstance, done <-chan struct{}) error {
+
+	// Receive the service
+	service := <-start
+
+	// Create the context with cancel
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// Call cancel in the background
+	go func() {
+		<-done
+		cancel()
+	}()
+
+	// Receive a stream of messages
+	stream, err := service.Receive(ctx, &pb.ReceiveRequest{})
+	if err != nil {
+		return err
+	}
+	for {
+		if message, err := stream.Recv(); err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		} else {
+			fmt.Printf("Event=%v\n", message)
+		}
+	}
+
+	// Success
+	return nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// MAIN
 
 func HasService(services []string, service string) bool {
 	if services == nil {
@@ -45,32 +88,23 @@ func HasService(services []string, service string) bool {
 func Main(app *gopi.AppInstance, done chan<- struct{}) error {
 
 	client := app.ModuleInstance("rpc/client").(gopi.RPCClientConn)
+	start = make(chan pb.MiHomeClient)
 
 	if services, err := client.Connect(); err != nil {
 		return err
 	} else if has_service := HasService(services, "MiHome"); has_service == false {
-		return fmt.Errorf("Invalid MiHome Gateway address (services are %v)", strings.Join(services, ","))
-	} else if service_obj, err := client.NewService(reflect.ValueOf(pb.NewMiHomeClient)); err != nil {
+		return fmt.Errorf("Invalid MiHome gateway address (missing service)")
+	} else if obj, err := client.NewService(reflect.ValueOf(pb.NewMiHomeClient)); err != nil {
 		return err
-	} else if service, ok := service_obj.(pb.MiHomeClient); service == nil || ok == false {
-		_ = service_obj.(pb.MiHomeClient)
+	} else if service, ok := obj.(pb.MiHomeClient); service == nil || ok == false {
 		return errors.New("Invalid MiHome service")
 	} else {
-		// Receive a stream of messages
-		if stream, err := service.Receive(context.Background(), &pb.ReceiveRequest{}); err != nil {
-			return err
-		} else {
-			for {
-				if message, err := stream.Recv(); err == io.EOF {
-					break
-				} else if err != nil {
-					return err
-				} else {
-					fmt.Printf("Event=%v\n", message)
-				}
-			}
-		}
+		// Send the service to the receive loop
+		start <- service
 	}
+
+	// Wait for signal
+	app.WaitForSignal()
 
 	// Finish gracefully
 	done <- gopi.DONE
@@ -84,5 +118,5 @@ func main() {
 	config := gopi.NewAppConfig("rpc/client")
 
 	// Run the command line tool
-	os.Exit(gopi.CommandLineTool(config, Main))
+	os.Exit(gopi.CommandLineTool(config, Main, ReceiveLoop))
 }
